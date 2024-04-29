2fc35cf3d06e5d4220eabf2d5553108cb238fc2c
sandesh thapa <sandeshthapa@sandeshs-MacBook-Pro.local>
2024-04-29 20:18:13 +0900

    changes

--- src/app/modules/auth/login/data-access/models/confirm-security-code.model.ts
+++ src/app/modules/auth/login/data-access/models/confirm-security-code.model.ts
@@ -2,21 +2,18 @@ import { FormInputInterface } from "../../../../../shared/utils/interface";
 
 export const CONFIM_SECURITY_CODE_FORM_FIELD : Array<FormInputInterface> = [
       {
-        name: 'password',
-        label: 'password',
-        type: 'password',
+        name: 'confirm_verification_code',
+        label: 'Verification Code',
+        type: 'text',
         id: 'password',
-        placeholder: 'enter security code',
+        placeholder: '-',
         className: 'form-control',
         validation: {
           required: {
             value: true,
             message: 'required',
           },
-          maxLength: {
-            value: 30,
-            message: '30 characters max',
-          },
+         
         },
       }
 ];
