3bff9e1ceffaab2206a6e745870baf6b5e55a9ed
Sandesh Thapa <letsmailsandesh@gmail.com>
2024-05-02 10:04:21 +0900

    layout changes updated

--- src/app/modules/auth/dashboard-raw/DashboardRawPage.tsx
+++ src/app/modules/auth/dashboard-raw/DashboardRawPage.tsx
@@ -27,15 +27,33 @@ function DashboardRawPage() {
                          
                             <div className="nav-bar-item-right hide-on-phone">
                                 <div className="profile">
-                                    <div className="icon">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
-                                            <path stroke-linecap="round" stroke-linejoin="round" d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 0 1-.825-.242m9.345-8.334a2.126 2.126 0 0 0-.476-.095 48.64 48.64 0 0 0-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0 0 11.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155" />
-                                        </svg>
-                                    </div>
-                                    {/* Add other icons */}
+                                <div className="icon">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                    stroke="currentColor" className="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 0 1-.825-.242m9.345-8.334a2.126 2.126 0 0 0-.476-.095 48.64 48.64 0 0 0-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0 0 11.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155" />
+                                </svg>
+    
+                            </div>
+                            <div className="icon">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                    stroke="currentColor" className="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
+                                </svg>
+    
+                            </div>
+                            <div className="icon">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                    stroke="currentColor" className="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                </svg>
+    
+    
+                            </div>
                                 </div>
                             </div>
-                            <button className="fixed-button" onClick={() => toggleCurtainMenu()}>Menu <i className="fa fa-arrow-circle-o-right"></i></button>
                         </div>
                     </div>
                 </section>
@@ -222,25 +240,21 @@ function DashboardRawPage() {
                                 </div>
     
                                 {/* Search and Filter */}
-                                <div className="search-device-type-selection mtb-15">
-                                <div className="device-type">
-                                <div className="form">
-                                    <div className="input-group">
+                            <div className="search-device-type-selection mtb-15">
+                            <div className="device-type">
+                                <div className="form">                                   
                                         <select name="" className="form-control" id="">
                                             <option value="">Device Type</option>
-                                        </select>
-
-                                    </div>
+                                        </select>                                    
                                 </div>
                             </div>
                             <div className="inventory-search">
-                                <div className="form">
-                                    <div className="input-group">
-                                        <input type="text" name="" id="" placeholder="search"/>                                        
-                                    </div>
+                                <div className="form">                                   
+                                        <input type="text" name="" id="" placeholder="search"/>                                    
+                                    
                                 </div>
                             </div>
-                                </div>
+                        </div>
     
                                 {/* Suggested layout */}
                                 <div className="suggested-layout-wrapper mtb-30">
