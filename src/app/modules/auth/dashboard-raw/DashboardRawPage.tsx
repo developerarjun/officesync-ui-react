5d7eb313c24e7043bde4d1e3de9c6fb099ae89c5
Sandesh Thapa <letsmailsandesh@gmail.com>
2024-05-02 10:13:38 +0900

    more changes

--- src/app/modules/auth/dashboard-raw/DashboardRawPage.tsx
+++ src/app/modules/auth/dashboard-raw/DashboardRawPage.tsx
@@ -49,9 +49,7 @@ function DashboardRawPage() {
                                     <path stroke-linecap="round" stroke-linejoin="round"
                                         d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                                 </svg>
-    
-    
-                            </div>
+                                </div>
                                 </div>
                             </div>
                         </div>
@@ -64,6 +62,7 @@ function DashboardRawPage() {
             <section className="dashboard-content">
                 <div className="container">
                     <div className="dashboard-inner-wrapper">
+                        {/* sidebar start */}
                         <div className="sidebar hide-on-phone">
                             <div className="sidebar-content">
                             <div className="sidebar-title">
@@ -232,7 +231,7 @@ function DashboardRawPage() {
                         </div>
                             </div>
                         </div>
-    
+                        {/* sidebar ends */}
                         <div className="main-content">
                             <div className="main-content-wrapper ">
                                 <div className="page-title">
